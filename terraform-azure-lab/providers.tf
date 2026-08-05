02d9839dda276e4b1049dacc882a85114310e854
EndIsNearR <teas01_rigor@icloud.com>
2026-08-05 18:01:33 +0500

    added code for plan and apply

--- terraform-azure-lab/providers.tf
+++ terraform-azure-lab/providers.tf
@@ -4,7 +4,6 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    # Use the storage account already created by this Terraform deployment.
     resource_group_name  = "rg-data"
     storage_account_name = "staz104lab08app"
     container_name       = "tfstate"
